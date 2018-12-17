cf914d906eaffef0502dbb1911497520578fea52
LRG3 <lrg2013@yahoo.com>
2018-12-16 23:45:59 -0700

    update final

--- keys.js
+++ keys.js
@@ -6,15 +6,16 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
-  
+
+ 
   // Client ID 4427f96cd27d449e8f042d9a9cea7362
   // Client Secret b45bc0b6031846ceaad927bd02f0ed33
   
   
-  // module.exports = {
-  //     exports: spotify = keys
+   //module.exports = {
+    // exports: spotify = keys
       
-  //       };
+       //  };
 
    // OMDB
-   var omdbKey = 'd83756ee';
+   //var omdbKey = 'd83756ee';
